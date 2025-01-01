89422af09fb30d74e38c318c7f065e6f94127e95
Makkusu <maxchu92@hotmail.com>
2025-01-02 00:29:54 +0800

    Added version number to footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -2,7 +2,7 @@ export default function AppFooter() {
   return (
     <footer>
       <div className="container mx-auto text-muted-foreground text-sm flex justify-between p-8">
-        <p>Max Chu | Portfolio</p>
+        <p>Max Chu | Portfolio v{process.env.version}</p>
         <p className="">&copy;&nbsp;Max Chu&nbsp;{new Date().getFullYear()}</p>
       </div>
     </footer>
