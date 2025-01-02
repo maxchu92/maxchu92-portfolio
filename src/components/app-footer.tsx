fa160befb93a339d87062f2e08a642f7d61ad90c
Max Chu <maxchu92@hotmail.com>
2025-01-02 20:54:09 +0800

    Updated images for projects

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -8,7 +8,7 @@ export default function AppFooter() {
         <Contacts />
       </div>
       <div className="container mx-auto text-muted-foreground text-sm flex justify-between p-8">
-        <p>Max Chu | Portfolio v{process.env.version}</p>
+        <p>maxchu92 | Portfolio v{process.env.version}</p>
         <p className="">&copy;&nbsp;Max Chu&nbsp;{new Date().getFullYear()}</p>
       </div>
     </footer>
