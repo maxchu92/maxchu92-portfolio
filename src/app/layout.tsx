15abccbd8d98253a679103577853a78ced112fca
Makkusu <maxchu92@hotmail.com>
2025-01-06 02:51:00 +0800

    fixed lint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import MultiProvider from '@/components/multi-provider';
-import profile from '@/data/profile.json';
+//import profile from '@/data/profile.json';
 
 const poppins = Poppins({
   subsets: ['latin'],
