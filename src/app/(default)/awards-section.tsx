d037f03b21eb23fe84bf5a03469ff3ab2414e4c3
Makkusu <maxchu92@hotmail.com>
2025-01-05 23:06:02 +0800

    updated activities and awards

--- src/app/(default)/awards-section.tsx
+++ src/app/(default)/awards-section.tsx
@@ -13,7 +13,7 @@ export default function AwardsSection() {
     >
       <SectionHeader title="Awards" />
       <div className="container mx-auto py-8 px-4">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
           <AspectRatio ratio={1}>
             <AnimationOnScroll
               classNameInView="animate-in fade-in slide-in-from-right duration-1000 h-full w-full"
@@ -55,6 +55,21 @@ export default function AwardsSection() {
                 </Card>
               </AnimationOnScroll>
             ))}
+          <AspectRatio ratio={1}>
+            <AnimationOnScroll
+              classNameInView="animate-in fade-in slide-in-from-right duration-1000 h-full w-full"
+              classNameNotInView="opacity-0"
+            >
+              <div className="bg-muted rounded-xl">
+                <Image
+                  fill={true}
+                  src="/images/awards/dew-challenge-2012.jpg"
+                  alt="Profile Picture"
+                  className="rounded-xl object-cover"
+                />
+              </div>
+            </AnimationOnScroll>
+          </AspectRatio>
         </div>
       </div>
     </div>
